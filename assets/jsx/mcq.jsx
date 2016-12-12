b5edfbda8442bf72d33aed89acb1fe40bc7b0af1
Rahul <polrahul10@gmail.com>
2016-12-12 11:11:27 +0530

    Option setup in progress

--- assets/jsx/mcq.jsx
+++ assets/jsx/mcq.jsx
@@ -1,7 +1,52 @@
 
 var Option = React.createClass({
+
+    getInitialState: function () {
+        return { selected: false }
+    },
+
+    onTickClicked:function(){
+        this.setState({ selected: !this.state.selected });
+    },//onTickClicked
+
+    componentDidMount: function(){
+        CKEDITOR.replace(this.props.optionId);
+    },//componentDidMount
+
     render:function(){
-        return (<div id={this.props.optionId}></div>);
+
+        var value = "";
+        if(this.props.action == "create"){
+            value = "";
+        }
+        else if(this.props.action == "edit"){
+            //call db and get option value
+            value= "this is from db";
+        }
+
+        var optionButtonClass = "btn btn-sm btn-default";
+        if (this.state.selected == true) {
+            optionButtonClass = "btn btn-sm btn-success";
+        } else {
+            optionButtonClass = "btn btn-sm btn-default";
+        }
+
+        return (
+            <div className="option-body col-md-6">
+                <div className="option-header">
+                    <span className="option-title">({this.props.optionText})</span>
+                    <span className="pull-right"> 
+                        <button  className={optionButtonClass} onClick={this.onTickClicked}>
+                            <i className="fa fa-check" style={{ paddingRight: 10 + 'px' }}></i>mark this option as answer.
+                        </button> 
+                    </span>                    
+                </div>
+                <div className="option-text">
+                    <textarea id={this.props.optionId} defaultValue={value}  rows="5" cols="50">
+                    </textarea>
+                </div>
+            </div>
+        );
     }//render
 });
 
@@ -21,38 +66,24 @@ var OptionSet = React.createClass({
             //call db with question id as this.props.questionId and get the count
             return 6;
         }
-    },//optionCount
-
-    eachOption: function (i,text,value) {        
-        return (
-            <Option key={i} optionId={text} optionValue={value}>                
-            </Option>
-        );
-    },
-
-    renderNew:function(optionCount){               
-        for(var i=0; i<optionCount; i++){
-            return this.eachOption(i,"option-"+this.state.optionText[i],"");
-        }        
-    },//renderNew
-
-    renderFromDB:function(optionCount){
-        //retrieve data from db and call eachOption with value
-        return <div></div>;
-    },//renderFromDB
-
-    shouldComponentUpdate: function () {        
-        return false;
-    },//shouldComponentUpdate
+    },//optionCount    
 
     render:function(){
         var optionCount = this.optionCount();
-        if(this.props.action == "create"){
-            return this.renderNew(optionCount);
+        var options = [];
+        for(var i=0; i<optionCount; i++) {
+            var optionId = "option-" + this.state.optionText[i];
+            options.push(<Option key={i} optionText={this.state.optionText[i]} optionId={optionId} questionId={this.props.questionId} action={this.props.action}></Option>);
         }
-        else if(this.props.action == "edit"){
-            return this.renderFromDB(optionCount);
-        }
+
+        return(
+            <div className="option-set col-md-12">
+                <div className="option-title col-md-12" style={{ marginLeft: -15 + 'px' }}> Options </div>  
+                <div className="option-content">              
+                    {options}
+                </div>
+            </div>
+        );
     }//render
 });
 
@@ -86,15 +117,17 @@ window.Board = React.createClass({
 
     render:function(){
         return(
-            <div className="mcq-board well well-bg">
+            <div className="mcq-board well well-bg col-md-12">
                 <div className="question-header">
                     <span className="question-title">Question</span>
                     <span className="pull-right"> 
                         <button className="btn btn-success">Save</button> 
                     </span>
                 </div>
-                <div className="question">
-                    <Question action={this.props.action} questionId={this.props.questionId}></Question>
+                <div className="question col-md-12">
+                    <Question action={this.props.action} questionId={this.props.questionId}></Question>                    
+                </div>
+                <div className="option row">
                     <OptionSet action={this.props.action} questionId={this.props.questionId}></OptionSet>
                 </div>
 
