221698627afac84389a3297f4d7acaf3e903570e
Rahul Pol <polrahul10@gmail.com>
2016-12-13 21:54:07 +0530

    CKEditor Modification

--- assets/js/ckeditor/build-config.js
+++ assets/js/ckeditor/build-config.js
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/6b4a8106d96d7565ee3a1bd0cf1f2534
+ * (2) http://ckeditor.com/builder/c9e7b50f44036b7b2bc10686e4bf7234
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/6b4a8106d96d7565ee3a1bd0cf1f2534
+ * (3) http://ckeditor.com/builder/download/c9e7b50f44036b7b2bc10686e4bf7234
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -25,7 +25,7 @@
  */
 
 var CKBUILDER_CONFIG = {
-	skin: 'moono-lisa',
+	skin: 'icy_orange',
 	preset: 'standard',
 	ignore: [
 		'.bender',
@@ -65,13 +65,16 @@ var CKBUILDER_CONFIG = {
 		'horizontalrule' : 1,
 		'htmlwriter' : 1,
 		'image' : 1,
+		'imageuploader' : 1,
 		'indentlist' : 1,
 		'link' : 1,
 		'list' : 1,
 		'magicline' : 1,
+		'mathjax' : 1,
 		'maximize' : 1,
 		'pastefromword' : 1,
 		'pastetext' : 1,
+		'pbckcode' : 1,
 		'removeformat' : 1,
 		'resize' : 1,
 		'scayt' : 1,
@@ -88,73 +91,6 @@ var CKBUILDER_CONFIG = {
 		'wysiwygarea' : 1
 	},
 	languages : {
-		'af' : 1,
-		'ar' : 1,
-		'bg' : 1,
-		'bn' : 1,
-		'bs' : 1,
-		'ca' : 1,
-		'cs' : 1,
-		'cy' : 1,
-		'da' : 1,
-		'de' : 1,
-		'de-ch' : 1,
-		'el' : 1,
-		'en' : 1,
-		'en-au' : 1,
-		'en-ca' : 1,
-		'en-gb' : 1,
-		'eo' : 1,
-		'es' : 1,
-		'et' : 1,
-		'eu' : 1,
-		'fa' : 1,
-		'fi' : 1,
-		'fo' : 1,
-		'fr' : 1,
-		'fr-ca' : 1,
-		'gl' : 1,
-		'gu' : 1,
-		'he' : 1,
-		'hi' : 1,
-		'hr' : 1,
-		'hu' : 1,
-		'id' : 1,
-		'is' : 1,
-		'it' : 1,
-		'ja' : 1,
-		'ka' : 1,
-		'km' : 1,
-		'ko' : 1,
-		'ku' : 1,
-		'lt' : 1,
-		'lv' : 1,
-		'mk' : 1,
-		'mn' : 1,
-		'ms' : 1,
-		'nb' : 1,
-		'nl' : 1,
-		'no' : 1,
-		'oc' : 1,
-		'pl' : 1,
-		'pt' : 1,
-		'pt-br' : 1,
-		'ro' : 1,
-		'ru' : 1,
-		'si' : 1,
-		'sk' : 1,
-		'sl' : 1,
-		'sq' : 1,
-		'sr' : 1,
-		'sr-latn' : 1,
-		'sv' : 1,
-		'th' : 1,
-		'tr' : 1,
-		'tt' : 1,
-		'ug' : 1,
-		'uk' : 1,
-		'vi' : 1,
-		'zh' : 1,
-		'zh-cn' : 1
+		'en' : 1
 	}
 };
--- assets/js/ckeditor/config.js
+++ assets/js/ckeditor/config.js
@@ -1,30 +1,37 @@
-/**
+﻿/**
  * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
 CKEDITOR.editorConfig = function (config) {
+
+	// %REMOVE_START%
+	// The configuration options below are needed when running CKEditor from source files.
+	config.plugins = 'dialogui,dialog,about,a11yhelp,basicstyles,blockquote,clipboard,panel,floatpanel,menu,contextmenu,resize,button,toolbar,elementspath,enterkey,entities,popup,filebrowser,floatingspace,listblock,richcombo,format,horizontalrule,htmlwriter,wysiwygarea,image,indent,indentlist,fakeobjects,link,list,magicline,maximize,pastetext,pastefromword,removeformat,showborders,sourcearea,specialchar,menubutton,scayt,stylescombo,tab,table,tabletools,undo,wsc,imageuploader,lineutils,widgetselection,widget,mathjax,pbckcode';
+	config.skin = 'icy_orange';
+	// %REMOVE_END%
+
 	// Define changes to default configuration here.
 	// For complete reference see:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
-	// The toolbar groups arrangement, optimized for two toolbar rows.
+	// The toolbar groups arrangement, optimized for two toolbar rows.	
 	config.toolbarGroups = [
 		{ name: 'clipboard', groups: ['clipboard', 'undo'] },
 		{ name: 'editing', groups: ['find', 'selection', 'spellchecker'] },
 		{ name: 'links' },
-		'/',
 		{ name: 'insert' },
 		{ name: 'forms' },
 		{ name: 'tools' },
 		{ name: 'document', groups: ['mode', 'document', 'doctools'] },
 		{ name: 'others' },
+		{ name: 'pbckcode' },
 		'/',
 		{ name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
 		{ name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi'] },
 		{ name: 'styles' },
 		{ name: 'colors' },
-		//{ name: 'about' }
+
 	];
 
 	// Remove some buttons provided by the standard plugins, which are
@@ -35,5 +42,38 @@ CKEDITOR.editorConfig = function (config) {
 	config.format_tags = 'p;h1;h2;h3;pre';
 
 	// Simplify the dialog windows.
-	//config.removeDialogTabs = 'image:advanced;link:advanced';
+	config.removeDialogTabs = 'image:advanced;link:advanced';
+
+	config.mathJaxLib = 'http://cdn.mathjax.org/mathjax/2.2-latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML';
+
+	// ADVANCED CONTENT FILTER (ACF)
+	// ACF protects your CKEditor instance of adding unofficial tags
+	// however it strips out the pre tag of pbckcode plugin
+	// add this rule to enable it, useful when you want to re edit a post
+	// Only needed on v1.1.x and v1.2.0
+	//config.allowedContent = 'pre[*]{*}(*)'; // add other rules here
+
+	// PBCKCODE CUSTOMIZATION
+	config.pbckcode = {
+		// An optional class to your pre tag.
+		cls: '',
+
+		// The syntax highlighter you will use in the output view
+		highlighter: 'PRETTIFY',
+
+		// An array of the available modes for you plugin.
+		// The key corresponds to the string shown in the select tag.
+		// The value correspond to the loaded file for ACE Editor.
+		modes: [['HTML', 'html'], ['CSS', 'css'], ['PHP', 'php'], ['JS', 'javascript'], ['JAVA', 'java'], ['C', 'c'], ['C++', 'c++']],
+
+		// The theme of the ACE Editor of the plugin.
+		theme: 'tomorrow_night',
+
+		// Tab indentation (in spaces)
+		tab_size: '4',
+
+		// the root path of ACE Editor. Useful if you want to use the plugin
+		// without any Internet connection
+		js: "https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.0/"
+	};
 };
